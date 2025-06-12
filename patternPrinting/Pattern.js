b1dc1ff50d870ab2a927c3547a7e5fae0682971b
Mohd-Affan-Code <mohdaffanjkhan@gamil.com>
2025-06-12 12:51:44 +0530

    pattern printing questin

---
+++ patternPrinting/Pattern.js
@@ -0,0 +1,16 @@
+// for (let a = 1; a <= 100; a = a + 10) {
+//   for (let b = a; b < a + 10; b++) {
+//     document.write(`${b} `);
+//   }
+//   document.write("<br>");
+// }
+
+// question no 2
+
+// for (let i = 1; i <= 4; i++) {
+//   console.log(i);
+//   for (let j = 1; j <= 3; j++) {
+//     document.write(" * ");
+//   }
+//   document.write("<br>");
+// }
