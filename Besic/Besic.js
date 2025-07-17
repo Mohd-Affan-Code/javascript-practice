753ebd8e151b197cec3dc1ff32b0ae33d8d47c40
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-07-17 21:48:49 +0530

    print even number

--- Besic/Besic.js
+++ Besic/Besic.js
@@ -3,3 +3,11 @@
 for (let i = 0; i <= 10; i++) {
   console.log(i);
 }
+
+// 2. Print even numbers between 1 and 20
+
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
