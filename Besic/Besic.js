1fa70eb31a27e49101e20559649f7f8f231b2a30
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-07-17 22:05:35 +0530

    new question print table

--- Besic/Besic.js
+++ Besic/Besic.js
@@ -1,13 +1,27 @@
 // 1. Print numbers from 1 to 10 using a for loop
 
-for (let i = 0; i <= 10; i++) {
-  console.log(i);
-}
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
 
 // 2. Print even numbers between 1 and 20
 
-for (let i = 0; i <= 20; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-}
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 3. Print odd numbers between 1 and 20
+
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 === 1) {
+//     console.log(i);
+//   }
+// }
+
+// 4. Print the multiplication table of 5
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`5 x ${i} = ${5 * i}`);
+// }
