948b894d365092fde2ebdf1240b54321089f0f2f
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-07-19 23:01:48 +0530

    print prime number

--- Besic/Besic.js
+++ Besic/Besic.js
@@ -83,3 +83,20 @@
 //   a = b;
 //   b = next;
 // }
+
+// 11. Print all prime numbers between 1 and 100
+
+for (let i = 2; i <= 100; i++) {
+  let isPrime = true;
+
+  for (let j = 2; j <= Math.sqrt(i); j++) {
+    if (i % j === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    console.log(i);
+  }
+}
