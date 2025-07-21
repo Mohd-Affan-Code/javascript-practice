93eda5350484997e24ab7d1b83fe3a689213f225
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-07-21 12:30:36 +0530

    array find smallest element

--- array/array.js
+++ array/array.js
@@ -1,12 +1,25 @@
 // 1. Find the largest element in an array
 
-const numbers = [10, 25, 5, 80, 45];
-let largest = numbers[0];
+// const numbers = [10, 25, 5, 80, 45];
+// let largest = numbers[0];
 
-for (let i = 1; i < numbers.length; i++) {
-  if (numbers[i] > largest) {
-    largest = numbers[i];
-  }
-}
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > largest) {
+//     largest = numbers[i];
+//   }
+// }
 
-console.log("Largest number is:", largest);
+// console.log("Largest number is:", largest);
+
+// 2.  Find the smallest element in an array
+
+// const numbers = [10, 25, 5, 80, 45];
+// let smallest = numbers[0];
+
+// for (let i = 1; i < numbers.length; i++) {
+//   if (smallest > numbers[i]) {
+//     smallest = numbers[i];
+//   }
+// }
+
+// console.log(smallest);
